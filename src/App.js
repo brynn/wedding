dbb9a3396a0129c192d8cfcac403ba4936ce26e8
Brynn Shepherd <brynn@google.com>
2022-09-17 13:43:50 -0400

    initial design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import photo from './img/ab.jpg';
+import logo from './img/logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="photo">
+        <img src={photo} alt="Andrew &amp; Brynn" />
+      </div>
+      <div className="content">
+        <div className="intro section">
+          <ul className="nav">
+            <li><a href="#story">Prelude</a></li>
+            <li><a href="#details">Details</a></li>
+            <li><a href="#travel">Travel &amp; Lodging</a></li>
+            <li><a href="#rsvp">RSVP</a></li>
+          </ul>
+          <div className="title">
+            <img src={logo} alt="Andrew &amp; Brynn" className="logo" />
+            <p className="location">Migis Lodge <br />South Casco, ME</p>
+            <p className="date">june third, two thousand &amp; twenty-three</p>
+          </div>
+
+        </div>
+        <div className="story section">
+          <p>Our story goes here...</p>
+        </div>
+        <div className="details section">
+          <p>Wedding details</p>
+        </div>
+        <div className="travel section">
+          <p>Travel info</p>
+        </div>
+        <div className="rsvp section">
+          <p>RSVP info</p>
+        </div>
+      </div>
     </div>
   );
 }
