039c9b322a00c4255a609a03a3e34f934703a740
Brynn Shepherd <brynn@google.com>
2022-10-01 14:45:29 -0400

    call backend

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import logo from './img/logo.png';
 import './App.css';
 
 const testApi = async () => {
-  const response = await fetch('wedding-be/api');
+  const response = await fetch('wedding-be/api/rsvp');
   console.log('response: ', response);
-}
+};
 
 function App() {
   testApi();
@@ -17,14 +17,29 @@ function App() {
       <div className="content">
         <div className="intro section">
           <ul className="nav">
-            <li><a href="#story">Prelude</a></li>
-            <li><a href="#details">Schedule <span className="amp">&amp;</span> Details</a></li>
-            <li><a href="#travel">Travel <span className="amp">&amp;</span> Lodging</a></li>
-            <li><a href="#rsvp">RSVP</a></li>
+            <li>
+              <a href="#story">Prelude</a>
+            </li>
+            <li>
+              <a href="#details">
+                Schedule <span className="amp">&amp;</span> Details
+              </a>
+            </li>
+            <li>
+              <a href="#travel">
+                Travel <span className="amp">&amp;</span> Lodging
+              </a>
+            </li>
+            <li>
+              <a href="#rsvp">RSVP</a>
+            </li>
           </ul>
           <div className="title">
             <img src={logo} alt="Andrew &amp; Brynn" className="logo" />
-            <p className="location">Migis Lodge <br />South Casco, Maine</p>
+            <p className="location">
+              Migis Lodge <br />
+              South Casco, Maine
+            </p>
             <p className="date">june third, two thousand &amp; twenty-three</p>
           </div>
         </div>
@@ -32,10 +47,14 @@ function App() {
           <p>Prelude</p>
         </div>
         <div className="details section">
-          <p>Schedule <span className="amp">&amp;</span> Details</p>
+          <p>
+            Schedule <span className="amp">&amp;</span> Details
+          </p>
         </div>
         <div className="travel section">
-          <p>Travel <span className="amp">&amp;</span> Lodging</p>
+          <p>
+            Travel <span className="amp">&amp;</span> Lodging
+          </p>
         </div>
         <div className="rsvp section">
           <p>RSVP</p>
