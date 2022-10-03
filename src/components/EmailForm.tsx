a9586488a0e7aefc0da0720f621440bc87ba5d10
Brynn Shepherd <brynn@google.com>
2022-10-03 16:25:06 -0400

    loading

--- src/components/EmailForm.tsx
+++ src/components/EmailForm.tsx
@@ -10,15 +10,23 @@ interface Props {
 const EmailForm: React.FC<Props> = ({setGuest}: Props) => {
   const [email, setEmail] = useState<string>('');
   const [error, setError] = useState<string>('');
+  const [loading, setLoading] = useState<boolean>(false);
 
   const confirmEmail = async (e: React.MouseEvent, email: string) => {
     e.preventDefault();
-    const response = await getGuest(email);
-    if ((response as Guest)?.id) {
-      // If the response has an id, we know we confirmed the guest
-      setGuest(response as Guest);
-    } else {
-      setError(response as string);
+    try {
+      setLoading(true);
+      const response = await getGuest(email);
+      if ((response as Guest)?.id) {
+        // If the response has an id, we know we confirmed the guest
+        setGuest(response as Guest);
+      } else {
+        setError(response as string);
+      }
+      setLoading(false);
+    } catch (err) {
+      setError(err);
+      setLoading(false);
     }
   };
 
@@ -36,7 +44,7 @@ const EmailForm: React.FC<Props> = ({setGuest}: Props) => {
         onChange={(e) => setEmail(e.target.value)}
       />
       <Button variant="contained" onClick={(e) => confirmEmail(e, email)} disabled={!email}>
-        Confirm Email
+        {loading ? 'Loading...' : 'Confirm Email'}
       </Button>
     </>
   );
--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,9 @@
 import {createTheme} from '@mui/material/styles';
 
 export const API_HOST =
-  process.env.NODE_ENV === 'production' ? 'https://wedding-be.onrender.com' : 'http://localhost:3001';
+  process.env.NODE_ENV === 'production'
+    ? 'https://wedding-be.onrender.com'
+    : 'http://localhost:3001';
 
 // TODO (brynn): use consts for colors
 
